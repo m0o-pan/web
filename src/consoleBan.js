446ac67f24d132fe00dce3171cfb9a099db571f0
m0o-pan <13784197582@163.com>
2023-09-04 17:45:57 +0800

    Update consoleBan.js

--- src/consoleBan.js
+++ src/consoleBan.js
@@ -1,1 +1,31 @@
+  const fireR = () => {
+      let a = 0
+      return () => {
+        if (++a === 2) {
+          console.log('really in ')
 
+          const dg = new Function('debugger')
+          setTimeout(() => {
+            dg()
+            location.href = 'about:blank'
+          }, 0)
+          a = 1
+        }
+      }
+    }
+
+    const errorDetector = () => {
+      const e = new Error()
+      let i = 0
+      const c = fireR()
+
+      Object.defineProperty(e, 'message', {
+        get () {
+          console.log('控制台打开', i++, new Date())
+          c()
+          //
+        }
+      })
+      console.log(e)
+    }
+    errorDetector()
